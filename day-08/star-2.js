17f702c40f6901f0966d08ca871bf46a85026944
Crosson Brieuc <crosson.brieuc@gmail.com>
2023-12-08 12:50:56 +0100

    brut force will not work for d6 s2

--- day-08/star-2.js
+++ day-08/star-2.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
 
-const map = {}
-
-
 if (process.argv.length < 3) {
     console.log("Usage: ", process.argv[0], process.argv[1], "<input file>")
     process.exit(1)
@@ -17,6 +14,9 @@ const directions = lines[0]
     .split("")
     .map(e => 0 + (e == "R")); // 1 for right , 0 for left
 
+const map = {}
+const mapKeys = []
+
 lines
     .slice(1)
     .map(l => l.split(" = "))
@@ -24,15 +24,22 @@ lines
         map[l[0]] = l[1]
             .slice(1, -1) //remove parenthesis
             .split(", ");
+        if (l[0][2] == "A")
+            mapKeys.push(l[0])
     });
 
-
+console.log(mapKeys)
 let dir_i = 0;
-let map_key = "AAA";
+let keysWithZ = 0;
 let steps = 0;
 
-while (map_key != "ZZZ") {
-    map_key = map[map_key][directions[dir_i]]
+while (keysWithZ != mapKeys.length) {
+    keysWithZ = 0
+    for (let i = 0; i < mapKeys.length; ++i) {
+        mapKeys[i] = map[mapKeys[i]][directions[dir_i]]
+        if (mapKeys[i][2] == "Z")
+            ++keysWithZ;
+    }
     steps++;
     dir_i = ++dir_i % directions.length
 }
